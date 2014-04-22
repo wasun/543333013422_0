74d2f0886cd325c128f6da7d7014a6849ff278ab
wasun <kuywan2499@hotmail.com>
2014-04-22 21:54:33 +0700

    rtyyyr

---
+++ Lab1 Constructor/src/Lab2/TestArrayList.java
@@ -0,0 +1,35 @@
+package Lab2;
+
+
+
+public class TestArrayList {
+
+	public static void main(String[] args) {
+		ArrayListQueue aq = new ArrayListQueue();
+		aq.enQueue("AA");
+		aq.enQueue("BB");
+		aq.enQueue("CC");
+		aq.enQueue("DD");
+		aq.enQueue("EE");
+
+		System.out.println(aq);
+		aq.deQueue();
+
+		System.out.println(aq);
+		System.out.println(aq.peek());
+
+
+		System.out.println("#################################");
+		ArrayList arr = new ArrayList();
+		arr.add("A");
+		System.out.println(arr);
+		arr.add("B");
+		System.out.println(arr);
+		arr.add("C");
+		System.out.println(arr);
+		arr.add(2, "D");
+		System.out.println(arr);
+		System.out.println("#################################");
+	}
+
+}
